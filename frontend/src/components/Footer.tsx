0e0ba20fbe68a26020d7a968b4ab43c9db76e0cc
mati <mateusz.meksula@gmail.com>
2024-07-11 18:26:55 +0200

    react-icons, social links

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,17 @@
 import { FC } from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer: FC = () => {
   return (
     <footer className="footer">
       <div className="creator">Created By Mati</div>
       <nav>
-        <a href="">GitHub</a>
-        <a href="">LinkedIn</a>
-        <a href="">Instagram</a>
+        <a href="https://github.com/mateusz-meksula">
+          <FaGithub style={{ marginRight: "1rem", fontSize: "1.5rem" }} />
+        </a>
+        <a href="https://www.linkedin.com/in/mateusz-meksuła-2397982b0">
+          <FaLinkedin style={{ fontSize: "1.5rem" }} />
+        </a>
       </nav>
     </footer>
   );
