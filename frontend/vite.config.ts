308d6be95c662b468ce15eabe2430448932e1af4
mati <mateusz.meksula@gmail.com>
2024-07-15 18:15:30 +0200

    backend proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://backend:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
