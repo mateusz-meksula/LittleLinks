b6bcbeee7501d6a261a1e278f7fe62f81df68dc8
mati <mateusz.meksula@gmail.com>
2024-07-16 21:37:56 +0200

    login functionality

--- frontend/src/components/LogInForm.tsx
+++ frontend/src/components/LogInForm.tsx
@@ -1,19 +1,82 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const LogInForm: FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const { setAuthContext } = useAuthContext();
+  const navigate = useNavigate();
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!username || !password) {
+      return;
+    }
+
+    const formData = new FormData(e.currentTarget);
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setAuthContext({
+        isUserLoggedIn: true,
+        username: username,
+        accessToken: data.token,
+      });
+      navigate("/");
+    } else if (response.status === 401) {
+      setUsernameError("Incorrect username or password");
+      setPasswordError("Incorrect username or password");
+    } else {
+      toast.error("Something went wrong, try again later", {
+        className: "toast",
+      });
+    }
+  }
+
   return (
     <section className="center-page">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <p className="form-title">Log in</p>
         <div className="form-field">
-          <label htmlFor="">username</label>
-          <input type="text" name="" id="" required />
-          <div className="form-field-error"></div>
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+              setUsernameError("");
+            }}
+            required
+          />
+          <div className="form-field-error">{usernameError}</div>
         </div>
         <div className="form-field">
-          <label htmlFor="">password</label>
-          <input type="password" name="" id="" required />
-          <div className="form-field-error"></div>
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setPasswordError("");
+            }}
+            required
+          />
+          <div className="form-field-error">{passwordError}</div>
         </div>
         <button type="submit">Log in</button>
       </form>
