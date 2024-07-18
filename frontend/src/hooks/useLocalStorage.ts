d93124dc3bd7a0badd8d8ed0c45034a47d2641c8
mati <mateusz.meksula@gmail.com>
2024-07-18 21:06:39 +0200

    useLocalStorage hook

---
+++ frontend/src/hooks/useLocalStorage.ts
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function getFromLocalStorage<T>(key: string): T | null {
+  const saved = localStorage.getItem(key);
+  if (saved === null) return null;
+  return JSON.parse(saved);
+}
+
+const useLocalStorage = <T>(key: string): [T | null, (v: T | null) => void] => {
+  const [value, setValue] = useState<T | null>(getFromLocalStorage(key));
+
+  function storeValue(v: T | null): void {
+    setValue(v);
+    localStorage.setItem(key, JSON.stringify(v));
+  }
+
+  return [value, storeValue];
+};
+
+export default useLocalStorage;
