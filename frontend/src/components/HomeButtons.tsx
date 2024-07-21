22a160f9f55c92cc471d1565ebe796ac306e0b5e
mati <mateusz.meksula@gmail.com>
2024-07-21 17:49:51 +0200

    home buttons styling & display

--- frontend/src/components/HomeButtons.tsx
+++ frontend/src/components/HomeButtons.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
 
 const HomeButtons: FC = () => {
+  const { authContext } = useAuthContext();
+
   return (
     <section className="center-page">
       <div className="home-buttons">
         <Link to="/create-link">
           <button>Create little link</button>
         </Link>
-        <button>My little links</button>
+        {authContext.isUserLoggedIn && <button>My little links</button>}
       </div>
     </section>
   );
